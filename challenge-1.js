f4cf5716685b53fd2eae981f31890e7b88bbfdd7
A. Simon <adryel@live.it>
2022-12-14 20:16:31 -0300

    solved 1

---
+++ challenge-1.js
@@ -0,0 +1,18 @@
+console.log("Challenge 1:")
+
+const array = [2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]
+
+console.log(array.join(", "))
+
+const fromLeft = (array) => {
+    for (let i in array) {
+        if (array[i] === 1) {
+            array.unshift(array.splice(i, 1))
+        }
+    }
+}
+
+fromLeft(array)
+console.log(array.join(", "))
+
+console.log("--------------------")
